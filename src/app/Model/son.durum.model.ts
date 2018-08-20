966c45c79c99f808974d8673a5d1646cef53ef60
veysel <vinanc72@gmail.com>
2018-08-20 03:34:07 +0300

    Son durum model added

---
+++ src/app/Model/son.durum.model.ts
@@ -0,0 +1,25 @@
+export class SonDurumModel {
+    aktuelBasinc: number;
+    denizSicaklik: number;
+    denizeIndirgenmisBasinc: number;
+    gorus: number;
+    hadiseKodu: string;
+    istNo: number;
+    kapalilik: number;
+    karYukseklik: number;
+    nem: number;
+    rasatMetar: string;
+    rasatSinoptik: string;
+    rasatTaf: string;
+    ruzgarHiz: number;
+    ruzgarYon: number;
+    sicaklik: number;
+    veriZamani: string;
+    yagis00Now: number;
+    yagis10Dk: number;
+    yagis12Saat: number;
+    yagis1Saat: number;
+    yagis24Saat: number;
+    yagis6Saat: number;
+    denizVeriZamani: string;
+}
